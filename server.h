e0366ca2bf6088da3ce0954f78d090311e393dc6
Quentin Clrt <46262211+QuentinClrt@users.noreply.github.com>
2020-04-09 09:44:50 +0200

    Delete server.h

--- server.h
+++
@@ -1,9 +0,0 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-
-#define PORT 8080
